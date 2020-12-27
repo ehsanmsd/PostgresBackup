d554561909d5d8d6f6e6cd03a6f880ef603c9c1e
azimmsd <azimmasoudi@gmail.com>
2020-12-27 04:19:50 +0330

    Structure project with setup.py and Makefile

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='UTF-8') as f:
+    readme = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    description='Database backups locally or to AWS S3.',
+    long_description=readme,
+    author='Azim Masoudi',
+    author_email='azimmasoudi@gmail.com',
+    install_requires=[],
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
