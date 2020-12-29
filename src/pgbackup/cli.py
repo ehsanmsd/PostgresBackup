11d21ad131fe40473ac99ad29302358deed3b0c5
azimmsd <azimmasoudi@gmail.com>
2020-12-29 16:09:54 +0330

    Implement CLI Module

---
+++ src/pgbackup/cli.py
@@ -0,0 +1,19 @@
+from argparse import ArgumentParser, Action
+known_drivers = ['local' , 's3']
+class DriverAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        driver, destination = values
+        if driver.lower() not in known_drivers:
+            parser.error("Unknown driver. Available drivers are 'local' & 's3'")
+        namespace.driver = driver.lower()
+        namespace.destination = destination
+
+def create_parser():
+    parser = ArgumentParser()
+    parser.add_argument('url', help="URL of the PostgreSQL database to backup")
+    parser.add_argument('--driver',
+            help="how & where to share the backup",
+            nargs=2,
+            action=DriverAction,
+            required=True)
+    return parser
